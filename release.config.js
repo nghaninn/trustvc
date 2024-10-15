2797fc481e7d64f5508ad3f36a5b074a98156c0f
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-10-15 13:45:10 +0800

    fix: prepare for first release

---
+++ release.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  repositoryUrl: 'https://github.com/TrustVC/trustvc.git',
+  branches: [
+    {
+      name: 'main',
+      release: true,
+    },
+    {
+      name: 'v1',
+      prerelease: 'alpha',
+    },
+  ],
+  github: true,
+  changelog: true,
+  npm: true,
+  outputPath: '/dist',
+  buildTarget: 'build',
+  tagFormat: 'v${version}',
+  debug: true,
+  plugins: [
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+      },
+    ],
+  ],
+  releaseRules: [
+    {
+      subject: '*BREAKING CHANGE*',
+      release: 'major',
+    },
+    {
+      type: 'feat',
+      release: 'minor',
+    },
+    {
+      type: 'fix',
+      release: 'patch',
+    },
+    {
+      type: 'perf',
+      release: 'patch',
+    },
+  ],
+};
