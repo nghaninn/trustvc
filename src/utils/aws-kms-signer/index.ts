975f82e19ea1376f9a3b1bd77ca44cf843adbecd
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2025-06-09 15:46:21 +0800

    feat: add aws kms signer (#68)

---
+++ src/utils/aws-kms-signer/index.ts
@@ -0,0 +1,2 @@
+export { AwsKmsSigner } from '@tradetrust-tt/ethers-aws-kms-signer';
+export type { AwsKmsSignerCredentials } from '@tradetrust-tt/ethers-aws-kms-signer';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,4 @@ export * from './supportedChains';
 export * from './errorMessages';
 export * from './documents';
 export * from './gasStation';
+export * from './aws-kms-signer';
