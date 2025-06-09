c4e191de42d2a53430cd8d44896b3b6ad7a8a0c5
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2025-06-09 17:06:19 +0800

    fix: expose contracts (#69)

--- src/token-registry-v4/contracts.ts
+++ src/token-registry-v4/contracts.ts
@@ -3,10 +3,14 @@ export {
   TitleEscrowFactory__factory,
   TitleEscrow__factory,
   TradeTrustToken__factory,
+  TradeTrustTokenStandard__factory,
+  ERC1967UpgradeUpgradeable__factory as ERC1967__factory,
 } from '@tradetrust-tt/token-registry-v4/contracts';
 export type {
   TDocDeployer,
   TitleEscrow,
   TitleEscrowFactory,
   TradeTrustToken,
+  TradeTrustTokenStandard,
+  ERC1967UpgradeUpgradeable as ERC1967,
 } from '@tradetrust-tt/token-registry-v4/contracts';
--- src/token-registry-v5/contracts.ts
+++ src/token-registry-v5/contracts.ts
@@ -3,10 +3,14 @@ export {
   TitleEscrowFactory__factory,
   TitleEscrow__factory,
   TradeTrustToken__factory,
+  TradeTrustTokenStandard__factory,
+  ERC1967Utils__factory as ERC1967__factory,
 } from '@tradetrust-tt/token-registry-v5/contracts';
 export type {
   TDocDeployer,
   TitleEscrow,
   TitleEscrowFactory,
   TradeTrustToken,
+  TradeTrustTokenStandard,
+  ERC1967Utils as ERC1967,
 } from '@tradetrust-tt/token-registry-v5/contracts';
